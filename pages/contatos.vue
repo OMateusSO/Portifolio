e2d65f93ba0d478ba50dc83f5676acbd1886de65
Dali <mateuswy123@gmail.com>
2024-04-29 20:04:31 -0300

    sixth commit

--- pages/contatos.vue
+++ pages/contatos.vue
@@ -5,40 +5,49 @@ import { lightDark } from '../sharedTheme';
 <template>
   <Nav></Nav>
   <div :class="{ 'dark-theme': lightDark }" class="flex items-center justify-center h-screen">
-    <div class="w-[350px] h-auto p-4 border bg-white text-black border-gray-300 rounded-lg">
-      <div class="flex items-center mb-4">
-        <div class="mr-4">
-          <img src="/assets/img/github.png" class="w-16 h-18" alt="GitHub">
+    <div>
+      <h1 class="text-2xl text-center">Links de contatos</h1>
+      <div class="w-[350px] h-auto p-4 border bg-white text-black border-gray-300 rounded-lg mx-auto">
+        <div class="flex items-center mb-4">
+          <div class="mr-4">
+            <a target="_blank" href="https://github.com/OMateusSO?tab=repositories">
+              <img src="/assets/img/github.png" class="w-16 h-18" alt="GitHub">
+            </a>
+          </div>
+          <div>
+            <a target="_blank" href="https://github.com/OMateusSO?tab=repositories"
+              class="hover:text-blue-600">Github</a>
+          </div>
         </div>
-        <div>
-          <a target="_blank" href="https://github.com/OMateusSO?tab=repositories" class="hover:text-blue-600">Github</a>
+        <div class="flex items-center mb-4">
+          <div class="mr-4">
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=mateuswy123@gmail.com&su=">
+              <img src="/assets/img/gmail.png" class="w-16 h-18" alt="Gmail">
+            </a>
+          </div>
+          <div>
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=mateuswy123@gmail.com&su="
+              class="hover:text-blue-600">
+              Gmail
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="flex items-center mb-4">
-        <div class="mr-4">
-          <img src="/assets/img/gmail.png" class="w-16 h-18" alt="Gmail">
-        </div>
-        <div>
-          <a target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&to=mateuswy123@gmail.com&su=" 
-          class="hover:text-blue-600">
-            Gmail
-          </a>
-        </div>
-      </div>
-      <div class="flex items-center">
-        <div class="mr-4">
-          <img src="/assets/img/linkedin.png" class="w-16 h-18" alt="LinkedIn">
-        </div>
-        <div>
-          <a target="_blank" href="https://www.linkedin.com/in/mateus-sousa-basilio-a0097b217/" 
-          class="hover:text-blue-600">
-            LinkedIn
-          </a>
+        <div class="flex items-center">
+          <div class="mr-4">
+            <a target="_blank" href="https://www.linkedin.com/in/mateus-sousa-basilio-a0097b217/">
+              <img src="/assets/img/linkedin.png" class="w-16 h-18" alt="LinkedIn">
+            </a>
+          </div>
+          <div>
+            <a target="_blank" href="https://www.linkedin.com/in/mateus-sousa-basilio-a0097b217/"
+              class="hover:text-blue-600">
+              LinkedIn
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <style>
